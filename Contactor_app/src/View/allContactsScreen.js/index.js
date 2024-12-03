47e5d08d48d9922db45da277a5edbd2c40f2f1c8
telmathorhalls <telmat23@ru.is>
2024-12-03 10:27:43 +0000

    commenting who has what so we don't forget or get confused like last time

--- Contactor_app/src/View/allContactsScreen.js/index.js
+++ Contactor_app/src/View/allContactsScreen.js/index.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import { View, Text, TouchableHighlight, Image, TouchableOpacity, FlatList, Alert } from "react-native";
 import style from "./styles";
 
+//Bjarni
+
 const allContacts = ({ navigation: {navigate}}) => {
     console.log("asdlkjqiowdjiopqwdkpoqwdkopqwk")
     return(
--- Contactor_app/src/View/chosenContactScreen.js/index.js
+++ Contactor_app/src/View/chosenContactScreen.js/index.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import { View, Text, TouchableHighlight, Image, TouchableOpacity, FlatList, Alert } from "react-native";
 import style from "./styles";
 
+//Manwin
+
 const chosenContact = ({ navigation: {navigate}}) => {
 
 };
