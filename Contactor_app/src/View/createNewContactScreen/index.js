07fc7633349d678243e83de61999c135d8809ec5
Manwin <manwin8@gmail.com>
2024-12-03 13:05:13 +0000

    deleted extra files.. dont worry it still works

--- Contactor_app/src/View/createNewContactScreen/index.js
+++
@@ -1,7 +0,0 @@
-import React, {useState} from "react";
-import { View, Text, TouchableHighlight, Image, TouchableOpacity, FlatList, Alert } from "react-native";
-import style from "./styles";
-
-const newContact = ({ navigation: {navigate}}) => {
-
-};
