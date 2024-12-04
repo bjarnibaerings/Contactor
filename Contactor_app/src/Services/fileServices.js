0da438c331d4b0e0c7fa88aec3a7b908de112743
bjarnibaerings <bjarnibh@outlook.com>
2024-12-04 11:50:11 +0000

    Added default image if no image is given

--- Contactor_app/src/Services/fileServices.js
+++ Contactor_app/src/Services/fileServices.js
@@ -43,6 +43,9 @@ Contacts
 */
 //Add a new contact
 export const addContact = async (contactInformation) =>{
+    if (contactInformation.image === "" && contactInformation.image === false){
+        contactInformation.image = "https://static.vecteezy.com/system/resources/previews/003/715/527/non_2x/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-vector.jpg"
+    }
     const fileUri = contactDirectory +"/"+contactInformation.name + "-" + contactInformation.id +".json";
     const jsonContents = JSON.stringify(contactInformation);
     fileSystem.writeAsStringAsync(fileUri,jsonContents);
