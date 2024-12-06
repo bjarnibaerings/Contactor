b2fca7586d4df030a0b06603db68a72ee6593858
bjarnibaerings <bjarnibh@outlook.com>
2024-12-06 11:46:01 +0000

    sjhg

--- Contactor_app/src/Services/fileServices.js
+++ Contactor_app/src/Services/fileServices.js
@@ -50,6 +50,9 @@ export const addContact = async (contactInformation) =>{
         
         contactInformation.image = "https://static.vecteezy.com/system/resources/previews/003/715/527/non_2x/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-vector.jpg"
     }
+    else{
+        contactInformation.image
+    }
     const fileUri = contactDirectory +"/"+contactInformation.name + "-" + contactInformation.id +".json";
     console.log(contactInformation.image)
     const jsonContents = JSON.stringify(contactInformation);
